65144cf09e541f3b2148227764012a1c66fae45e
Francesco Ruvolo <ruvolof@gmail.com>
2024-02-26 21:51:27 +0100

    Renaming variable becayse this is not bash.

--- app/src/main/java/com/werebug/androidnetcat/NetcatSession.kt
+++ app/src/main/java/com/werebug/androidnetcat/NetcatSession.kt
@@ -27,8 +27,8 @@ class NetcatSession : AppCompatActivity(), View.OnClickListener {
         }
         ncCmdArgv.removeAt(0)
         ncCmdArgv.add(0, ncatPath)
-        val bashWrappedArgv = listOf("/system/bin/sh", "-c", ncCmdArgv.joinToString(" "))
-        worker = NetcatWorker(bashWrappedArgv, WeakReference(this))
+        val shellWrappedArgv = listOf("/system/bin/sh", "-c", ncCmdArgv.joinToString(" "))
+        worker = NetcatWorker(shellWrappedArgv, WeakReference(this))
         worker.start()
 
         binding.btnSendText.setOnClickListener(this);
